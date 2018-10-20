01b9f85e777ced97a525da90b9d1b94a1ee42c42
remidev <28935481+remidev@users.noreply.github.com>
2018-10-19 21:55:49 -0400

    added game js and improved styles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,5 +38,63 @@
           - losses++
           - reinitialize game
 */
+var $userScore = $("#userScore");
+var $targetScore = $("#targetScore");
+var $crystalContainer = $("#crystals");
+var $wins = $("#wins");
+var $losses = $("#losses");
 
+var targetScore = -1;
+var userScore = 0;
+var wins = 0;
+var losses = 0;
+var imgSrcArr = [
+                "assets/images/green_crystal-128.png",
+                "assets/images/orange_crystal-128.png",
+                "assets/images/crystal_turquoise-128.png",
+                "assets/images/pink_crystal-128.png",
+                "assets/images/yellow_crystal-128.png"
+                ]
+
+function initGame() {
+
+  userScore = 0;
+  $userScore.text(userScore);
+
+  targetScore = Math.floor(Math.random() * 102) + 19;
+  $targetScore.text(targetScore);
+
+  $wins.text(wins);
+  $losses.text(losses);
+
+  $crystalContainer.empty();
+
+  for (i = 0; i < 5; i++) {
+    var crystal = $("<div>");
+    var crystalVal = Math.floor(Math.random() * 12) + 1;
+    crystal.attr({"class":"crystal","data": crystalVal });
+    crystal.css({"background-image":"url('" + imgSrcArr[i] + "')"})
+    $crystalContainer.append(crystal);
+  }
+}
+
+initGame();
+
+$(document).on("click",".crystal", function () {
+
+  userScore += parseInt($(this).attr("data"));
+  $userScore.text(userScore);
+
+  if (userScore === targetScore) {
+    wins++;
+    $wins.text(wins);
+    initGame();
+  }
+  if (userScore > targetScore) {
+    losses++;
+    $losses.text(losses);
+    initGame();
+  }
+
+});
 
